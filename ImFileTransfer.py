ff5053c544589e8232b4c7cbacf019616b4f3ee0
RasHerlo <rhj2111@columbia.edu>
2024-01-23 18:50:02 -0500

    "ImFileTransfer" working, also with Jupyter

--- ImFileTransfer.py
+++ ImFileTransfer.py
@@ -7,14 +7,21 @@ from tkinter import simpledialog
 
 def copy_files_with_substring_and_interval():
 
-    # Input information from dialog-windows
+    # enter all info with input-dialogs
+    import tkinter as tk
+    from tkinter import filedialog
+    from tkinter import simpledialog
+    from tkinter import messagebox
+    import os
+    import shutil
+
     root = tk.Tk()
     root.withdraw()
 
     input_path = filedialog.askdirectory(title="Select a Directory with imaging files")
-    input_substr = simpledialog.askstring("Input Dialog", "Enter your string:")
-    input_lowerbound = simpledialog.askinteger("Input Dialog", "Enter lower-bound index:")
-    input_upperbound = simpledialog.askinteger("Input Dialog", "Enter upper-bound index:")
+    input_substr = simpledialog.askstring("Input Dialog", "Enter your substring:")
+    lowerbound = simpledialog.askinteger("Input Dialog", "Enter lower-bound index:")
+    upperbound = simpledialog.askinteger("Input Dialog", "Enter upper-bound index:")
     target_dir = filedialog.askdirectory(title="Select target Directory for folder with imaging files")
     target_fld = simpledialog.askstring("Input Dialog", "Enter name of the folder for your images:")
 
@@ -26,6 +33,31 @@ def copy_files_with_substring_and_interval():
     root.destroy()  # Close the hidden window
 
     # Identify the files and confirm the transfer
+    file_paths = [
+        filename
+        for filename in os.listdir(input_path)
+        if input_substr in filename
+    ]
+    file_paths = file_paths[lowerbound:upperbound]
+    msg_process = f"""The first file transferred will be: {file_paths[0]}, and the last will be {file_paths[-1]}.
+                  Do you want to proceed?"""
+
+    root = tk.Tk()
+    root.withdraw()
+
+    answer = messagebox.askyesno("Confirmation", msg_process)
+
+    root.destroy()
+
+    # if answer is yes, start the transfer
+    if answer:
+        target_new = os.path.join(target_dir, target_fld)
+        os.mkdir(target_new)
+
+        for imfile in file_paths:
+            source_file = os.path.join(input_path, imfile)
+            destination_file = os.path.join(target_new, imfile)
+            shutil.copy2(source_file, destination_file)
 
 
 
